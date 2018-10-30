36561d5dc4c1f3bcde1dec7da5bd1af4b264965e
OLEG <tokaroleg@mail.ru>
2018-10-30 00:26:37 -0400

    8й урок - обратный таймер

---
+++ LESSON 8/js/script.js
@@ -0,0 +1,76 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+    'use strict';
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) { // -----  чтобы скрыть все эл-ты нашей страницы
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show'); // скрыать полностью со страницы
+            tabContent[i].classList.add('hide'); //
+        }
+    }
+
+    hideTabContent(1); // скрываются все кроме 1го
+
+    function showTabContent(b) { // ------  и показывается то что нам нужно
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function (event) { // делигирование ; куда кликать - targen
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) { // с пом цикла проверяется чобы название темы совпадало с информацией
+                if (target == tab[i]) { // все! табы возьмем и переберем сравнив с тем что мы кликнули
+                    hideTabContent(0); // таргет - куда мы тыкаем - скрываем все табы
+                    showTabContent(i); //  показываем тот кот совпадает
+                    break;
+                }
+            }
+        }
+    });
+
+
+    // Timer
+
+    let deadLine = '2018-10-21';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
+
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('timer', deadLine);
+});
