d01b2a8041df1c82abfbbafa2664063f6b3f8a8b
OLEG <tokaroleg@mail.ru>
2018-10-31 00:53:53 -0400

    9й урок и 8й исправленный

--- LESSON 8/js/script.js
+++ LESSON 8/js/script.js
@@ -41,12 +41,16 @@ window.addEventListener('DOMContentLoaded', function () {
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),
-            seconds = Math.floor((t / 1000) % 60),
+            days = Math.floor((t / (1000 * 60 * 60 * 24))),
+            // hours = Math.floor((t / (1000 * 60 * 60))),
+            hours = Math.floor(((t / 1000 / 60) % 60)),
             minutes = Math.floor((t / 1000 / 60) % 60),
-            hours = Math.floor((t / (1000 * 60 * 60)));
+            seconds = Math.floor((t / 1000) % 60);
+
 
         return {
             'total': t,
+            'days': days,
             'hours': hours,
             'minutes': minutes,
             'seconds': seconds
@@ -55,6 +59,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     function setClock(id, endtime) {
         let timer = document.getElementById(id),
+            days = timer.querySelector('.days'),
             hours = timer.querySelector('.hours'),
             minutes = timer.querySelector('.minutes'),
             seconds = timer.querySelector('.seconds'),
@@ -62,15 +67,39 @@ window.addEventListener('DOMContentLoaded', function () {
 
         function updateClock() {
             let t = getTimeRemaining(endtime);
-            hours.textContent = t.hours;
-            minutes.textContent = t.minutes;
-            seconds.textContent = t.seconds;
+
+            if (t.days < 10) {
+                days.textContent = '0' + t.days;
+            } else {
+                days.textContent = t.days;
+            }
+
+            if (t.hours < 10) {
+                hours.textContent = '0' + t.hours;
+            } else {
+                hours.textContent = t.hours;
+            }
+
+            if (t.minutes < 10) {
+                minutes.textContent = '0' + t.minutes;
+            } else {
+                minutes.textContent = t.minutes;
+            }
+
+            if (t.seconds < 10) {
+                seconds.textContent = '0' + t.seconds;
+            } else {
+                seconds.textContent = t.seconds;
+            }
+
+
 
             if (t.total <= 0) {
+
+                seconds.textContent = '00'
                 clearInterval(timeInterval);
             }
         }
     }
-
     setClock('timer', deadLine);
 });
