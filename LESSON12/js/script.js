7a126f653d97abcf60d840628190a6b4993d8034
OLEG <tokaroleg@mail.ru>
2018-11-08 00:17:31 -0500

    12й урок с ошибками

---
+++ LESSON12/js/script.js
@@ -0,0 +1,271 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+    'use strict';
+    const tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    const hideTabContent = (a) => { // -----  чтобы скрыть все эл-ты нашей страницы
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show'); // скрыать полностью со страницы
+            tabContent[i].classList.add('hide'); //
+        }
+    };
+
+    hideTabContent(1); // скрываются все кроме 1го
+
+    const showTabContent = (b) => { // ------  и показывается то что нам нужно
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    };
+
+    info.addEventListener('click', (event) => { // делигирование ; куда кликать - targen
+        const target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) { // с пом цикла проверяется чобы название темы совпадало с информацией
+                if (target == tab[i]) { // все! табы возьмем и переберем сравнив с тем что мы кликнули
+                    hideTabContent(0); // таргет - куда мы тыкаем - скрываем все табы
+                    showTabContent(i); //  показываем тот кот совпадает
+                    break;
+                }
+            }
+        }
+    });
+
+
+    // Timer
+
+    const deadLine = '2018-11-10';
+
+    const getTimeRemaining = (endtime) => {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor((t / (1000 * 60 * 60 * 24))),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / 1000 / 60 / 60) % 24);
+
+        if (days < 10) {
+            days = "0" + days;
+        }
+
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    };
+
+    const setClock = (id, endtime) => {
+        let timer = document.getElementById(id),
+            days = timer.querySelector('.days'),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds');
+
+        let updateClock = () => {
+            let t = getTimeRemaining(endtime);
+            if (t.total <= 0) {
+                days.textContent = '00';
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+
+            } else {
+
+                days.textContent = t.days;
+                hours.textContent = t.hours;
+                minutes.textContent = t.minutes;
+                seconds.textContent = t.seconds;
+            }
+        };
+        let timeInterval = setInterval(updateClock, 1000);
+    };
+
+    setClock('timer', deadLine);
+
+    // Modal   --- новый ES6 стандарт
+    const more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', () => {
+        overlay.style.display = 'block';
+        // this.classList.add('more-splash');
+        more.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', () => {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+
+    // Modal - Узнать подробнее  - новый ES6 стандарт
+    const descriptionBtn = document.querySelector(".description-btn"),
+        descriptionBtnCollection = document.getElementsByClassName("description-btn");
+
+    for (var i = 0; i < descriptionBtnCollection.length; i++) {
+        descriptionBtnCollection[i].addEventListener("click", () => {
+            overlay.style.display = "block";
+            more.classList.add("more-splash");
+            document.body.style.overflow = "hidden";
+        });
+    }
+    // Form    - различные состояния нашего запроса 
+
+    let message = {
+        loading: 'Загрузка...', // 3 разных свойства - 1я когда еще не обработалась 
+        success: 'Спасибо! Скоро мы с вами свяжемся!', // 2е свойство
+        failure: 'Что-то пошло не так...' // 3е свойство
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = document.getElementsByTagName('input'),
+        // inputPhone = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div'),
+        contactForm = document.querySelector('.form-contact'),
+         btnFormTwo = document.querySelector('#form');
+        // inputContactForm = contactForm.getElementsByTagName('input'),
+        // mainModal = document.querySelector('.popup'),
+        // thanksModal = document.querySelector('.thanks');
+    statusMessage.classList.add('status');
+
+    function sendForm(elem) {
+        elem.addEventListener('submit', function (e) {
+            e.preventDefault();
+            elem.appendChild(statusMessage);
+            let formData = new FormData(elem);
+
+            function postData(data) {
+                return new Promise(function (resolve, reject) {
+                    let request = new XMLHttpRequest();
+                    request.open('POST', 'server.php');
+                    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                    request.onreadystatechange = function () {
+                        if (request.readyState < 4) {
+                            resolve()
+                        } else if (request.readyState === 4) {
+                            if (request.status == 200 && request.status < 300)
+                                resolve();
+                        } else {
+                            reject();
+                        }
+                    }
+
+                });
+                request.send(data);
+
+
+            }
+        }); // End postData
+
+        function clearInput() {
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = '';
+            }
+        }
+
+        postData(formData)
+            .then(() => {
+                statusMessage.style.backgroundImage = message.loading;
+
+            })
+
+            .then(() => {
+                statusMessage.style.backgroundImage = message.success;
+                
+            })
+            .catch(() => statusMessage.style.backgroundImage = message.failure)
+            .then(clearInput)
+    }
+
+
+    sendForm(form);
+    sendForm(contactForm);
+});
+
+
+
+// form.addEventListener('submit', function (event) {
+//     event.preventDefault();
+//     form.appendChild(statusMessage);
+
+//     let request = new XMLHttpRequest();
+//     request.open('POST', 'server.php');
+//     request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+//     let formData = new FormData(form);
+//     let obj = {};
+//     formData.forEach(function (value, key) {
+//         obj[key] = value;
+//     });
+//     let json = JSON.stringify(obj);
+
+//     request.send(json);
+
+
+// request.addEventListener('readystatechange', function () {
+//     if (request.readyState < 4) {
+//         statusMessage.innerHTML = message.loading;
+//     } else if (request.readyState === 4 && request.status == 200) {
+//         statusMessage.innerHTML = message.success;
+//     } else {
+//         statusMessage.innerHTML = message.failure;
+//     }
+// });
+// for (let i = 0; i < input.length; i++) {
+//     input[i].value = '';
+// }
+
+
+// Form 2 -  контактная форма
+// let btnFormTwo = document.querySelector('#form');
+// let btnFormTwo = document.getElementById('form');    обязательно попробовать!
+
+// btnFormTwo.addEventListener('submit', function (event) {
+//     event.preventDefault();
+//     btnFormTwo.appendChild(statusMessage);
+
+//     let request = new XMLHttpRequest();
+//     request.open('POST', 'server.php');
+//     request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+//     let formData = new FormData(btnFormTwo);
+//     let obj = {};
+//     formData.forEach(function (value, key) {
+//         obj[key] = value;
+//     });
+//     let json = JSON.stringify(obj);
+
+//     request.send(json);
+
+
+//     request.addEventListener('readystatechange', function () {
+//         if (request.readyState < 4) {
+//             statusMessage.innerHTML = message.loading;
+//         } else if (request.readyState === 4 && request.status == 200) {
+//             statusMessage.innerHTML = message.success;
+//         } else {
+//             statusMessage.innerHTML = message.failure;
+//         }
+//     });
+//     for (let i = 0; i < input.length; i++) {
+//         input[i].value = '';
+//     }
+// });
+// });
