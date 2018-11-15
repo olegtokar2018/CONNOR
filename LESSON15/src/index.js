e26222f7ac90bd2bf9e89833db70f4f4323fc8c1
OLEG <tokaroleg@mail.ru>
2018-11-15 02:07:34 -0500

    15й урок исправленный

--- LESSON15/src/index.js
+++ LESSON15/src/index.js
@@ -1,12 +1,12 @@
 window.addEventListener('DOMConentLoaded', function () {
 
     'use strick';
-    let tabs = require('.parts/tabs.js'),
-        timer = require('.parts/timer.js'),
-        modal = require('.parts/modal.js'),
-        form = require('.parts/form.js'),
-        slider = require('.parts/slider.js'),
-        calc = require('.parts/calc.js');
+    let tabs = require('./parts/tabs.js'),
+        timer = require('./parts/timer.js'),
+        modal = require('./parts/modal.js'),
+        form = require('./parts/form.js'),
+        slider = require('./parts/slider.js'),
+        calc = require('./parts/calc.js');
 
     tabs();
     timer();
